4591e491729d546fb8d6590bc068f3e38ae2a70a
pranav718 <raypranav718@gmail.com>
2026-08-15 16:08:56 +0530

    feat: accept incoming request streams & add proxy-addr flag and start http proxy alongside control server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/pranav718/tunneru/internal/server"
@@ -10,6 +11,7 @@ import (
 
 var (
 	controlAddr string
+	proxyAddr   string
 	domain      string
 )
 
@@ -18,9 +20,18 @@ var rootCmd = &cobra.Command{
 	Short: "tunneru tunnel server: expose local ports to the internet",
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("tunneru server v0.1.0")
+
 		cs := server.NewControlServer(controlAddr, domain)
+		proxy := server.NewHTTPProxy(proxyAddr, domain, cs.Registry())
+
+		go func() {
+			if err := proxy.Start(); err != nil {
+				log.Fatalf("proxy server error: %v", err)
+			}
+		}()
+
 		if err := cs.Start(); err != nil {
-			fmt.Fprintf(os.Stderr, "error: %v\n", err)
+			fmt.Fprintf(os.Stderr, "control server error: %v\n", err)
 			os.Exit(1)
 		}
 	},
@@ -28,6 +39,7 @@ var rootCmd = &cobra.Command{
 
 func init() {
 	rootCmd.Flags().StringVar(&controlAddr, "control-addr", ":7001", "address for the control server")
+	rootCmd.Flags().StringVar(&proxyAddr, "proxy-addr", ":8080", "address for the public HTTP proxy server")
 	rootCmd.Flags().StringVar(&domain, "domain", "tunneru.dev", "base domain for tunnel URLs")
 }
 
--- internal/client/tunnel.go
+++ internal/client/tunnel.go
@@ -4,20 +4,16 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"time"
 
+	"github.com/pranav718/tunneru/internal/mux"
 	"github.com/pranav718/tunneru/internal/proto"
-)
-
-const (
-	pingInterval = 30 * time.Second
 )
 
 type Tunnel struct {
 	serverAddr  string
 	localPort   int
 	subdomain   string
-	conn        net.Conn
+	forwarder  *Forwarder
 }
 
 func NewTunnel(serverAddr string, localPort int, subdomain string) *Tunnel {
@@ -25,75 +21,39 @@ func NewTunnel(serverAddr string, localPort int, subdomain string) *Tunnel {
 		serverAddr: serverAddr,
 		localPort:  localPort,
 		subdomain:  subdomain,
+		forwarder:  NewForwarder(localPort),
 	}
 }
 
 func (t *Tunnel) Connect() error {
-	var err error
-	t.conn, err = net.Dial("tcp", t.serverAddr)
+	conn, err := net.Dial("tcp", t.serverAddr)
 	if err != nil {
 		return fmt.Errorf("tunnel connect: %w", err)
 	}
 	log.Printf("connected to server at %s", t.serverAddr)
 
-	defer func() {
-		t.conn.Close()
-		log.Printf("disconnected from server")
-	}()
+	defer conn.Close()
 
 	reg := &proto.Message{
 		Type:      proto.TypeRegister,
 		Subdomain: t.subdomain,
 	}
-	if err := proto.Encode(t.conn, reg); err != nil {
+	if err := proto.Encode(conn, reg); err != nil {
 		return fmt.Errorf("tunnel register send: %w", err)
 	}
 
-	resp, err := proto.Decode(t.conn)
-	if err != nil {
-		return fmt.Errorf("tunnel register response: %w", err)
-	}
+	session := mux.Client(conn)
+	defer session.Close()
 
-	switch resp.Type {
-	case proto.TypeRegistered:
-		log.Printf("tunnel active: %s", resp.URL)
-		log.Printf("forwarding to localhost:%d", t.localPort)
-	case proto.TypeError:
-		return fmt.Errorf("registration rejected: %s", resp.Error)
-	default:
-		return fmt.Errorf("unexpected response type: %s", resp.Type)
-	}
+	log.Printf("tunnel active: forwarding traffic from server to localhost:%d", t.localPort)
 
-	// start heartbeat ping loop in background
-	go t.pingLoop()
-
-	// message loop
 	for {
-		msg, err := proto.Decode(t.conn)
+		stream, err := session.AcceptStream()
 		if err != nil {
-			log.Printf("server read error: %v", err)
+			log.Printf("session closed: %v", err)
 			return nil
 		}
 
-		switch msg.Type {
-		case proto.TypePong:
-			// heartbeat acknowledged
-		default:
-			log.Printf("unhandled message type: %s", msg.Type)
-		}
-	}
-}
-
-// pingLoop sends a ping to the server every 30 seconds
-func (t *Tunnel) pingLoop() {
-	ticker := time.NewTicker(pingInterval)
-	defer ticker.Stop()
-
-	for range ticker.C {
-		ping := &proto.Message{Type: proto.TypePing}
-		if err := proto.Encode(t.conn, ping); err != nil {
-			log.Printf("ping send error: %v", err)
-			return
-		}
+		go t.forwarder.HandleStream(stream)
 	}
 }
