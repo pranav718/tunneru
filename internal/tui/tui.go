b9b3d66bf0f5f8c842727a65ec0ab01b1cfb189e
pranav718 <raypranav718@gmail.com>
2026-08-16 15:09:16 +0530

    added run entrypoint and stuff

--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -1,2 +1,25 @@
-// Package tui implements the Bubble Tea terminal dashboard.
 package tui
+
+import (
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type TUIConfig struct {
+	TunnelURL  string
+	LocalPort  int
+	InspectURL string
+}
+
+func Run(config TUIConfig, program **tea.Program) error {
+	model := NewModel(config.TunnelURL, config.LocalPort, config.InspectURL)
+	p := tea.NewProgram(model, tea.WithAltScreen())
+	*program = p
+	_, err := p.Run()
+	return err
+}
+
+func Send(program *tea.Program, event EventMsg) {
+	if program != nil {
+		program.Send(event)
+	}
+}
